88c55e3ca065fd01bd0789e8119bc586e3db013e
Daily-manjoo <yuslz35@gmail.com>
2023-12-23 00:47:59 +0900

    Firebase 세팅

---
+++ src/firebaseApp.ts
@@ -0,0 +1,22 @@
+
+import { initializeApp, FirebaseApp, getApp } from "firebase/app";
+
+export let app: FirebaseApp; 
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+
+try { //처음 초기화가 됐으면 getApp을 가져오고 아니면 초기화
+    app = getApp("app")
+} catch(e){
+    app= initializeApp(firebaseConfig, "app")
+}
+export const firebase = initializeApp(firebaseConfig);
+
+export default firebase;
