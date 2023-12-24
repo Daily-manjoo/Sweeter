217859933b4eb2db72d218372a655c6fae4f7cc8
Daily-manjoo <yuslz35@gmail.com>
2023-12-25 01:35:12 +0900

    Firebase 회원가입 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Router from 'components/Router';
 import { Layout } from 'components/Layout';
-import {getAuth} from "firebase/auth";
+import {getAuth, onAuthStateChanged} from "firebase/auth";
 import { app } from 'firebaseApp';
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const auth = getAuth(app);
+  const [init, setInit] = useState<boolean>(false);
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(!!auth?.currentUser);
-  console.log(auth, isAuthenticated);
+
+  useEffect(()=> {
+    onAuthStateChanged(auth, (user)=> {
+      if(user){
+        setIsAuthenticated(true);
+      } else{
+        setIsAuthenticated(false);
+      }
+      setInit(true); ///onAuthStateChanged이 실행되면 true로
+    })
+  }, [auth])
   return(
     <Layout>
-      <Router />
+      <ToastContainer />
+      { init ? <Router isAuthenticated={isAuthenticated} /> : 'loading' } {/*상태값 변경이 됐으면 보여주고 or loading */}
     </Layout>
   )
 }
