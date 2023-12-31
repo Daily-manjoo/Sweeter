db5040c7595276375a0ab8d56c50672f6921bc82
Daily-manjoo <yuslz35@gmail.com>
2023-12-31 18:50:13 +0900

    해쉬태그 폼 생성

--- src/components/posts/PostEditForm.tsx
+++ src/components/posts/PostEditForm.tsx
@@ -8,6 +8,8 @@ import { PostProps } from "pages/home";
 
 export default function PostEditForm() {
     const params = useParams(); //url에 id값을 가져와야 하기 때문
+    const [hashTag, setHashTag] = useState<string>("");
+    const [tags, setTags] = useState<string[]>([]);
     const [post, setPost] = useState<PostProps | null>(null);
     const [content, setContent ] = useState<string>("");
     const navigate = useNavigate();
@@ -22,6 +24,7 @@ export default function PostEditForm() {
             console.log(docSnap?.data(),docSnap.id)
             setPost({...(docSnap?.data() as PostProps), id: docSnap.id})
             setContent(docSnap?.data()?.content);
+            setTags(docSnap?.data()?.hashTags);
         }
     }, [params.id])
 
@@ -33,6 +36,7 @@ export default function PostEditForm() {
             const postRef = doc(db, "posts", post?.id);
             await updateDoc(postRef, {
                 content: content,
+                hashTags: tags,
             });
             navigate(`posts/${post?.id}`)
             toast.success("성공적으로 수정되었습니다.")
@@ -50,12 +54,41 @@ export default function PostEditForm() {
         }
     }
 
+    const removeTag = (tag:string) => {
+        setTags(tags?.filter((val) => val !== tag)) //value가 tag가 아닌 것만 필터링
+    }
+
+    const onChangeHashTag = (e: any) => {
+        setHashTag(e?.target?.value?.trim());
+    }
+
+    const handleKeyUp = (e:any) => {
+        if (e.keyCode === 32 && e.target.value.trim() !==''){
+            //엔터를 치고 빈 값이 없어야만 태그 생성
+            //만약 같은 태그가 있다면 에러를 띄운다
+            if(tags?.includes(e.target.value?.trim())) {
+                toast.error("같은 태그가 있습니다.");
+            } else {
+                setTags((prev) => (prev?.length > 0 ? [...prev,hashTag] : [hashTag]));
+                setHashTag("");
+            }
+        }
+    }
+
     useEffect(()=> {
         if(params?.id) getPost();
     }, [getPost, params?.id])
     return(
         <form className="post-form" onSubmit={onSubmit}>
         <textarea className="post-form__textarea" required name="content" id="content" placeholder="What is your dessert?" onChange={onChange} value={content} />
+        <div className="post-form__hashtags">
+        <span className="post-form__hastags-outputs">
+            {tags?.map((tag, index)=> (
+                <span className="post-form__hashtags-tag" key={index} onClick={() => removeTag(tag)}>#{tag}</span>
+            ))}
+        </span>
+            <input type="text" className="post-form__input" name="hashtag" id="hasgtag" placeholder="해시태그 + 스페이스 바 입력" onChange={onChangeHashTag} onKeyUp={handleKeyUp} value={hashTag} />
+        </div>
         <div className="post-form__submit-area">
             <label htmlFor="file-input" className="post-form__file">
                 <FiImage className="post-form__file-icon" />
