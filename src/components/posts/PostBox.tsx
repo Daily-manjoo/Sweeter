37051c520feb77990b0f051ca137987aa2464a0c
Daily-manjoo <yuslz35@gmail.com>
2024-01-12 01:03:54 +0900

    팔로우, 팔로잉 취소 db 설계

--- src/components/posts/PostBox.tsx
+++ src/components/posts/PostBox.tsx
@@ -9,6 +9,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { ref, deleteObject } from "firebase/storage";
 import { storage } from "firebaseApp";
+import FollowingBox from "components/following/FollowingBox";
 
 interface PostBoxProps {
     post: PostProps;
@@ -57,29 +58,36 @@ export default function PostBox({post}: PostBoxProps){
 
     return(
         <div className="post__box" key={post?.id}>
-            <Link to={`/posts/${post?.id}`}>
+            
                 <div className="post__box-profile">
                     <div className="post__flex">
                         {post?.profileUrl ? (<img src={post.profileUrl} alt="profile" className="post__boc-profile-img" /> 
                         ) : (
                         <FaUserCircle    className="post__box-profile-icon" /> 
                         )}
-                        <div className="post__email">{post?.email}</div>
-                        <div className="post__createdAt">{post?.createdAt}</div>
-                    </div>
-                    <div className="post__box-content">{post?.content}</div>
-                    {post?.imageUrl && (
-                        <div className="post__image-div">
-                            <img src={post?.imageUrl} alt="post img" className="post__image" width={100} height={100} />
+                        <div className="post__flex--between">
+                            <div className="post__flex">
+                                <div className="post__email">{post?.email}</div>
+                                <div className="post__createdAt">{post?.createdAt}</div>
+                            </div>
+                            <FollowingBox post={post} />
                         </div>
-                    )}
-                    <div className="post-form__hashtags-outputs">
-                        {post?.hashTags?.map((tag, index) => (
-                            <span className="post-form__hashtags-tag" key={index}>#{tag}</span>
-                        ))}
                     </div>
+                    <Link to={`/posts/${post?.id}`}>
+                        <div className="post__box-content">{post?.content}</div>
+                        {post?.imageUrl && (
+                            <div className="post__image-div">
+                                <img src={post?.imageUrl} alt="post img" className="post__image" width={100} height={100} />
+                            </div>
+                        )}
+                        <div className="post-form__hashtags-outputs">
+                            {post?.hashTags?.map((tag, index) => (
+                                <span className="post-form__hashtags-tag" key={index}>#{tag}</span>
+                            ))}
+                        </div>
+                    </Link>
                 </div>
-            </Link>
+            
             <div className="post__box-footer">
                 {user?.uid === post?.uid && (
                 <>
